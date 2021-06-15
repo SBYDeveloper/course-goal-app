b981a9d011795f4908264eb3873937c66f498d43
Stancho Yordanov <s.b.jordanov@gmail.com>
2021-06-15 20:06:37 +0300

    flexbox styling

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{borderColor: '#ccc', borderWidth: 1, padding: 10}}/>
+      <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{width: '80%',borderColor: '#ccc', borderWidth: 1, padding: 10}}/>
         <Button title="ADD"/>
       </View>
       <View>
